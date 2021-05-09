98bb75e1216c3e504b433d9d2f67f8c2033b4ebb
eyaMtss <68861017+eyaMtss@users.noreply.github.com>
2021-05-09 17:50:40 +0100

    Add files via upload

--- src/main/java/Proj/Spring/AppCoVacc19/GlobalExceptionHandler.java
+++ src/main/java/Proj/Spring/AppCoVacc19/GlobalExceptionHandler.java
@@ -4,13 +4,33 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import Proj.Spring.AppCoVacc19.Exception.AdminNotFoundException;
+import Proj.Spring.AppCoVacc19.Exception.CentreVaccNotFoundException;
+import Proj.Spring.AppCoVacc19.Exception.CitoyenNotFoundException;
+import Proj.Spring.AppCoVacc19.Exception.PersonnelNotFoundException;
+import Proj.Spring.AppCoVacc19.Exception.RendezVousNotFoundException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 	
 	
 	@ExceptionHandler(AdminNotFoundException.class)
-	public void handleAdministrateurNotFoundException(AdminNotFoundException e) {
+	public void handleAdminNotFoundException(AdminNotFoundException e) {
+		
+	}
+	@ExceptionHandler(CentreVaccNotFoundException.class)
+	public void handleCentreVaccNotFoundException(CentreVaccNotFoundException e) {
+		
+	}
+	@ExceptionHandler(CitoyenNotFoundException.class)
+	public void handleCitoyenNotFoundException(CitoyenNotFoundException e) {
+		
+	}
+	@ExceptionHandler(PersonnelNotFoundException.class)
+	public void handlePersonnelNotFoundException(PersonnelNotFoundException e) {
+		
+	}
+	@ExceptionHandler(RendezVousNotFoundException.class)
+	public void handleRendezVousNotFoundException(RendezVousNotFoundException e) {
 		
 	}
 	}
